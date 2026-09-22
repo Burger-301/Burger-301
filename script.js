c7af7d685a23bce78ce8d6a5d83e0ecf5f504fd9
cristianst3-lang <cristianst3@gmail.com>
2026-09-22 20:27:42 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -544,13 +544,14 @@ formularioPedido.addEventListener("submit", function (evento) {
     const telefone = "5551981061618";
     const url = `https://wa.me/${telefone}?text=${encodeURIComponent(mensagem)}`;
 
+    /* Abre o WhatsApp em nova aba */
     window.open(url, "_blank");
 
-    carrinho = [];
-    atualizarCarrinho();
-    formularioPedido.reset();
-});
-
+    /* Aguarda 1 segundo e recarrega a página zerada */
+    setTimeout(function () {
+        window.location.reload();
+    }, 1000);
+
 function mostrarMensagem(texto) {
     const mensagem = document.createElement("div");
     mensagem.className = "mensagem-sucesso";
