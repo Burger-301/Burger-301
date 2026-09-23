abe52cb96c059dce0f532860a879661ac64911f8
Bueger-301 <cristianst3@gmail.com>
2026-09-23 14:52:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ let quantidadeAtual = 1;
 // 1. ATIVAR / DESATIVAR BLOQUEIO DE HORÁRIO
 // true  = Bloqueia automaticamente fora de Sexta/Sábado (19h30 às 22h)
 // false = DESATIVA o bloqueio (permite fazer pedidos/testes a qualquer hora)
-const bloqueioHorarioAtivo = false;
+const bloqueioHorarioAtivo = true;
 
 // 2. LISTA DE PRODUTOS ESGOTADOS (Hambúrgueres e Porções)
 const produtosEsgotados = [
     // "Poema Kids",
     // "Smash 301",
-     "Clássico da Casa",
+    // "Clássico da Casa",
     // "Du'Chef",
     // "Poema Tropical",
     // "Porção de Fritas",
